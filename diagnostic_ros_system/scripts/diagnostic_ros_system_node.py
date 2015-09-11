72fe269b255210c3612a7f90bb19755632f8a35b
Elessog <elouan.autret@ensta-bretagne.org>
2015-09-11 19:28:48 +0200

    improve node = class

--- diagnostic_ros_system/scripts/diagnostic_ros_system_node.py
+++ diagnostic_ros_system/scripts/diagnostic_ros_system_node.py
@@ -2,6 +2,35 @@
 import rospy
 import rosnode
 import os
+from nav_msgs.msg import OccupancyGrid
+from std_msgs.msg import Float64
+
+class observed_node(object):
+     def __init__(self,name,msg_type,hertz=0.5):
+        self.name = name
+        self.hertz = hertz
+        self.subscriber = None
+        self.bool = 0
+        self.publisher =  rospy.Publisher("publ_name", Float64, queue_size=2)
+        self.msg_type = msg_type
+     def checkPub(self):
+        print "f"
+        self.subscriber = rospy.Subscriber(self.name, self.msg_type, self.callback)
+        rospy.sleep(rospy.sleep((1.0/self.hertz)*2))
+        self.subscriber.unregister()
+        if (self.bool<=0):
+           d = Float64()
+           d.data = 5
+           self.publisher.publish(d)
+        self.bool = 0
+
+     def pingNode(self):
+        res = rosnode.rosnode_ping(self.name,2,1)
+        return res
+
+     def callback(self,message):
+        self.bool = self.bool+1       
+         
 
 class diagnostic_node(object):
     def __init__(self):
@@ -21,30 +50,18 @@ class diagnostic_node(object):
             print line
         
 
-    def pingNode(self,node_name):
-        res = rosnode.rosnode_ping(node_name,2,1)
-
-    def checkVOChain(self):
-        print ""
-
-    def checkHSChain(self):
-        print ""
-
-    def checkOCVChain(self):
-        print ""
-
-    def checkControlChain(self):
-        print ""
-
     def spin(self):
-        rospy.spin()
+        n1 = observed_node("/map",OccupancyGrid,0.5)
+        while not rospy.is_shutdown():
+             n1.checkPub()
+             n1.pingNode()
 
 
 
 node = diagnostic_node()
 name = "car_calibration"
-node.pingNode("car_calibration")
-node.killNode("car_calibration")
-node.pingNode("car_calibration")
-#node.spin()
+#node.pingNode("car_calibration")
+#node.killNode("car_calibration")
+#node.pingNode("car_calibration")
+node.spin()
 
